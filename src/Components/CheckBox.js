2d0f343245afcbe5e2c936b41c21add19a1ac9a8
Sohail879 <97273586+Sohail879@users.noreply.github.com>
2022-05-23 11:17:35 +0530

    Add files via upload

---
+++ src/Components/CheckBox.js
@@ -0,0 +1,16 @@
+import Checkbox from '@mui/material/Checkbox';
+const CheckBox=(props)=>{
+ 
+    return(
+        <input
+        key={props.id}
+        onChange={props.checkboxHandler}
+        type="checkbox"
+        checked={props.data.isChecked}
+        value={props.data.id}
+      />
+            
+    )
+
+}
+export default CheckBox;
---
+++ src/Components/Search.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { margin } from '@mui/system';
+
+export default function Search(props) {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        maxWidth: '100%',
+       
+        
+        
+        
+      }}
+    >
+      <TextField fullWidth  id="fullWidth" placeholder='Search by name, email or role' onChange={(event)=>{props.handleSearch(event.target.value)}}/>
+    </Box>
+  );
+}
